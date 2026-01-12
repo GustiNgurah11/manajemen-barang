4da56f47267239cea2455c956d9315e2948e0f74
Aditya Febrian <adityaalfariq07@gmail.com>
2026-01-12 14:24:35 +0800

    menambahkan pembayaran untuk masing orang atau personal

--- app/Models/Pembayaran.php
+++ app/Models/Pembayaran.php
@@ -5,18 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Pembayaran
+ *
+ * Model ini merepresentasikan tabel pembayaran
+ * yang menyimpan data transaksi pembayaran.
+ */
 class Pembayaran extends Model
 {
+    // Trait untuk factory
     use HasFactory;
 
+    /**
+     * Nama tabel pada database
+     */
     protected $table = 'pembayaran';
 
+    /**
+     * Field yang dapat diisi secara mass assignment
+     */
     protected $fillable = [
-        'transaksi_id',
-        'tanggal_pembayaran',
-        'jumlah_bayar',
-        'metode_pembayaran',
-        'status_pembayaran'
+        'transaksi_id',        // ID transaksi terkait
+        'tanggal_pembayaran',  // Tanggal pembayaran
+        'jumlah_bayar',        // Nominal pembayaran
+        'metode_pembayaran',   // Metode pembayaran (cash, transfer, dll)
+        'status_pembayaran'    // Status pembayaran (lunas, pending, gagal)
     ];
 
     /**
