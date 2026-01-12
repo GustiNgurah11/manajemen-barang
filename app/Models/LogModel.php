0af43a3aafb1f83ae3e371f67ce6e82a6f515155
Aditya Febrian <adityaalfariq07@gmail.com>
2026-01-12 14:13:18 +0800

    ditambah coding (komentar & dokumentasi)

--- app/Models/LogModel.php
+++ app/Models/LogModel.php
@@ -6,22 +6,45 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Class LogModel
+ *
+ * Model ini digunakan untuk menyimpan aktivitas log aplikasi,
+ * seperti request, response, method, URL, dan IP user.
+ *
+ * Menggunakan Soft Delete sehingga data tidak langsung terhapus
+ * dari database.
+ */
 class LogModel extends Model
 {
+    // Trait untuk factory dan soft delete
     use HasFactory, SoftDeletes;
 
+    /**
+     * Nama tabel yang digunakan oleh model ini
+     */
     protected $table = 'log';
+
+    /**
+     * Primary key tabel log
+     */
     protected $primaryKey = 'log_id';
 
+    /**
+     * Field yang boleh diisi secara mass assignment
+     */
     protected $fillable = [
-        'user_id',
-        'log_method',
-        'log_url',
-        'log_ip',
-        'log_request',
-        'log_response'
+        'user_id',        // ID user yang melakukan aksi
+        'log_method',     // HTTP method (GET, POST, PUT, DELETE)
+        'log_url',        // URL endpoint yang diakses
+        'log_ip',         // IP address user
+        'log_request',    // Data request
+        'log_response'    // Data response
     ];
 
+    /**
+     * Field yang disembunyikan saat model dikonversi ke JSON
+     */
     protected $hidden = [
         'created_at',
         'updated_at',
